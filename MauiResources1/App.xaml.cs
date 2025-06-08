cd411aa055bd0edad6448abb32235b646b620d3e
Álvaro Neves <alvaro.neves2019@gmail.com>
2025-06-08 17:56:16 +0100

    Add project files.

---
+++ MauiResources1/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiResources1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage());
+        }
+    }
+}
